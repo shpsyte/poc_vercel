d84517f59e8ee349827e366bd57437a938da27f7
jose-luiz <jose.iscosistemas@gmail.com>
2022-06-12 20:50:07 -0700

    chagne folder name to API

--- webpack.server.js
+++ webpack.server.js
@@ -10,7 +10,7 @@ const config = {
 
   // Tell webpack the root file of our
   // server application
-  entry: './server',
+  entry: './api',
 
   // Tell webpack where to put the output file
   // that is generated
